4d3dd77df9eef4f06f4b625f302d2619c56a5884
anirbannayak999 <anirbannayak999@gmail.com>
2023-01-16 22:54:10 +0530

    stock problem

---
+++ src/com/bridgelabz/stockaccount.java
@@ -0,0 +1,48 @@
+package com.bridgelabz;
+
+public class stockaccount {
+    private String stockName;
+    private int noOfStock;
+    private double sharePrice;
+    private double stockValue;
+
+
+    public String getStockName() {
+        return stockName;
+    }
+
+
+    public void setStockName(String stockName) {
+        this.stockName = stockName;
+    }
+
+
+    public int getNoOfStock() {
+        return noOfStock;
+    }
+
+
+    public void setNoOfStock(int noOfStock) {
+        this.noOfStock = noOfStock;
+    }
+
+
+    public double getSharePrice() {
+        return sharePrice;
+    }
+
+
+    public void setSharePrice(double sharePrice) {
+        this.sharePrice = sharePrice;
+    }
+
+
+    public double getStockValue() {
+        return stockValue;
+    }
+
+
+    public void setStockValue(double stockValue) {
+        this.stockValue = stockValue;
+    }
+}
